7ee2332a9f4feecab595ec5d0955ae6898ee36b4
Grégoire M <gregoire@martinache.net>
2023-09-13 10:29:09 +0200

    Bump aarch64-image to Nixos 23.05

--- pkgs/aarch64-image/default.nix
+++ pkgs/aarch64-image/default.nix
@@ -7,8 +7,8 @@ stdenv.mkDerivation {
     # as hydra, since `pkgs.path` is embedded in the binary
     # To get a new url use:
     # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/release-20.03/nixos.sd_image.aarch64-linux/latest/download/1"
-    url = "https://hydra.nixos.org/build/214526899/download/1/nixos-sd-image-22.11.3408.a575c243c23-aarch64-linux.img.zst";
-    sha256 = "sha256-lYTksexy1Z9RJKtdZO1ufutLCPV4FmFVpF2alFMETEg=";
+    url = "https://hydra.nixos.org/build/142522657/download/1/nixos-sd-image-20.03.3330.1db42b7fe38-aarch64-linux.img.bz2";
+    sha256 = "sha256-fVzavDHMD8aibcQayBEpnIP382GFavsPIso9J1xx7vY=";
   };
   preferLocalBuild = true;
   dontUnpack = true;
